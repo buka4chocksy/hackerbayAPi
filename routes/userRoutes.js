f879dedfb30195c8cf0439469de4985f569db86d
chukwuebuka buka4chocksy <buka4chocksy@gmail.com>
2019-06-02 17:30:47 +0100

    required thumbnail controller and specified its routes with validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var routesValidator = require('../routesMiddleware/routesValidator');
 var userController = require('../Controllers/userController');
 var jsonPatchController = require('../Controllers/jsonPatchController');
+var thumbnailController = require('../Controllers/thumbnailController');
 router.post('/login',userController.login)
-router.patch('/jsonpatch' ,routesValidator.validateRoute, jsonPatchController.patch);
+router.patch('/jsonpatch' ,routesValidator.validateRoute, jsonPatchController.patch)
+router.post('/thumbnail', routesValidator.validateRoute, thumbnailController.thumbnailCreation)
 module.exports = router;
