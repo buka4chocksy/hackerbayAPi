202889c73be6ab9f514688f2f5bc25cc500569b8
chukwuebuka buka4chocksy <buka4chocksy@gmail.com>
2019-06-03 14:38:56 +0100

    modified json patch controller

--- Controllers/jsonPatchController.js
+++ Controllers/jsonPatchController.js
@@ -1,11 +1,11 @@
 var jsonpatch = require('jsonpatch');
 
 class jsonPatch{
-    patch(req,res,next){
+    patch(req,res){
         var mydoc = typeof(req.body.mydoc)=="string" ? JSON.parse(req.body.mydoc) : req.body.mydoc ;
         var patch = typeof(req.body.patch)=="string" ? JSON.parse(req.body.patch) : req.body.patch;
         mydoc = jsonpatch.apply_patch(mydoc,patch);
-        res.json({success:true , data:mydoc});
+        res.json(mydoc);
     }
 }
 
