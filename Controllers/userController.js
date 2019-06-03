56326ecbf9890e6c9a305f57574479824a29de77
chukwuebuka buka4chocksy <buka4chocksy@gmail.com>
2019-06-03 14:39:37 +0100

    modified user controller

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -6,14 +6,18 @@ class login{
             username: req.body.username,
             password:req.body.password
         }
-        token.generateToken(userDetails).then(userToken =>{
-            if(userToken == null){
-                console.log('i reach here');
-                res.json({message:'Sorry authentication failed'})
-            }else{
-                res.json({message:'Login Successful', token:userToken  })
-            }
-        }) 
+        if(userDetails.username ==''||  userDetails.password ==''){
+            res.status(400).send({ success: false , 'message':'Sorry you cant submit empty field' })  
+        }else{
+            token.generateToken(userDetails).then(userToken =>{
+                if(userToken == null){
+                    res.status(400).send({success:false, message:'Sorry authentication failed'})
+                }else{
+                    res.status(200).send({ success: true, message:'Login Successful', token:userToken  })
+                }
+            }) 
+        }
+       
      }   
 }
 module.exports = new login();
