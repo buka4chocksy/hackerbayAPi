ca678382392e678c75ee6c7a0601a0908797beea
chukwuebuka buka4chocksy <buka4chocksy@gmail.com>
2019-06-03 14:39:58 +0100

    modified token file

--- Validator/token.js
+++ Validator/token.js
@@ -1,10 +1,10 @@
 var jwt = require('jsonwebtoken');
-
+var secret = process.env.secret 
 class Token{
      generateToken(data={}){
         return new Promise((resolve, reject)=>{
             console.log('i am here');
-            jwt.sign({...data}, process.env.secret ,{expiresIn: '24hrs'}, function(err, token){
+            jwt.sign({...data},secret ,{expiresIn: '24hrs'}, function(err, token){
                 if(err){
                     reject(err);
                 }else{
@@ -15,9 +15,9 @@ class Token{
         });
     } 
     
-     verifyToken (token =""){
+     verifyToken (token){
         return new Promise((resolve, reject)=>{
-            jwt.verify(token.replace("Bearer",""), process.env.secret, function(err, verifiedToken){
+            jwt.verify(token.replace("bearer ", ""), secret, function(err, verifiedToken){
                 if(err){
                     reject(err);
                 }else{
@@ -27,5 +27,6 @@ class Token{
         });
     }
 }
+
 
 module.exports = new Token();
